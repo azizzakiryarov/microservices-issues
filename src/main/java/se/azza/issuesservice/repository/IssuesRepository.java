be443412fb55b51ca49bef8326523e7686501f70
Aziz Zakiryarov <aziz.zakiryarov@technia.com>
2019-08-10 10:15:33 +0200

    Added dependencies to pom.xml, @EnableEurikaClient, small changes on service, added name on application.properties for eurika server

--- src/main/java/se/azza/issuesservice/repository/IssuesRepository.java
+++ src/main/java/se/azza/issuesservice/repository/IssuesRepository.java
@@ -1,9 +1,13 @@
 package se.azza.issuesservice.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import se.azza.issuesservice.model.Issue;
 
 public interface IssuesRepository extends JpaRepository<Issue, Long> {
+	
+	List<Issue> findByUserId(Long id);
 
 }
--- src/main/java/se/azza/issuesservice/services/IssuesResources.java
+++ src/main/java/se/azza/issuesservice/services/IssuesResources.java
@@ -1,10 +1,13 @@
 package se.azza.issuesservice.services;
 
-import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +23,23 @@ public class IssuesResources {
 
 	@Autowired
 	IssuesRepository issuesRepository;
-	
+
+	@Autowired
+	RestTemplate restTemplate;
+
 	@RequestMapping("/add")
 	public ResponseEntity<Issue> addIssueToUser(@RequestParam(value = "comment") String comment,
 			@RequestParam(value = "state") String state, @RequestParam(value = "userId") long userId) {
-		
-		RestTemplate restTemplate = new RestTemplate();
-		User user = restTemplate.getForObject("http://localhost:8081/users/get/" + userId, User.class);
-		
+
+		User user = restTemplate.getForObject("http://microservices-users/users/get/" + userId, User.class);
 		Issue newIssue = new Issue(comment, state, user);
 		issuesRepository.save(newIssue);
 		return new ResponseEntity<Issue>(newIssue, HttpStatus.CREATED);
+	}
+
+	@GetMapping(path = "/get/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Issue>> getUserIssues(@PathVariable(value = "userId") long userId) {
+		List<Issue> issues = issuesRepository.findByUserId(userId);
+		return new ResponseEntity<List<Issue>>(issues, HttpStatus.OK);
 	}
 }
