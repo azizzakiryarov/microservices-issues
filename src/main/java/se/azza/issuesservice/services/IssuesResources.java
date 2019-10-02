7eecd3559cd16d5dbc0bdcfe21a02f798b8b1f21
Aziz Zakiryarov <aziz.zakiryarov@technia.com>
2019-10-02 20:25:56 +0200

    Added delete method, added @CrossOrigin

--- src/main/java/se/azza/issuesservice/services/IssuesResources.java
+++ src/main/java/se/azza/issuesservice/services/IssuesResources.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +32,7 @@ public class IssuesResources {
 	@Autowired
 	RestTemplate restTemplate;
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping("/add")
 	public ResponseEntity<Issue> addIssueToUser(@RequestParam(value = "comment") String comment,
 			@RequestParam(value = "userId") long userId) {
@@ -39,11 +42,13 @@ public class IssuesResources {
 		issuesRepository.save(newIssue);
 		return new ResponseEntity<Issue>(newIssue, HttpStatus.CREATED);
 	}
-	
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PutMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Issue> updateIssueForUser(@PathVariable(value = "id") Long id, @RequestParam(value = "userId") long userId, 
-			@RequestParam(value = "comment") String comment, @RequestParam(value = "issueState") issueState issueState){
-		
+	public ResponseEntity<Issue> updateIssueForUser(@PathVariable(value = "id") Long id,
+			@RequestParam(value = "userId") long userId, @RequestParam(value = "comment") String comment,
+			@RequestParam(value = "issueState") issueState issueState) {
+
 		User user = RestTemplates.getUserById(restTemplate, userId);
 		Optional<Issue> currentIssue = issuesRepository.findById(id);
 		Issue issue = new Issue(currentIssue.get().getId(), comment, issueState, user);
@@ -51,7 +56,33 @@ public class IssuesResources {
 		return new ResponseEntity<Issue>(issue, HttpStatus.OK);
 	}
 
-	@GetMapping(path = "/get/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@PutMapping(value = "/assignIssueTo/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Issue> assignIssueTo(@PathVariable(value = "id") Long id,
+			@RequestParam(value = "userId") long userId) {
+
+		User user = RestTemplates.getUserById(restTemplate, userId);
+		Optional<Issue> currentIssue = issuesRepository.findById(id);
+		Issue issue = new Issue(currentIssue.get().getId(), currentIssue.get().getComment(),
+				currentIssue.get().getIssueState(), user);
+		issuesRepository.save(issue);
+		return new ResponseEntity<Issue>(issue, HttpStatus.OK);
+	}
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@DeleteMapping(path = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Issue> deleteIssueForUser(@PathVariable(value = "id") Long id,
+			@RequestParam(value = "userId") String userId) {
+		List<Issue> issues = issuesRepository.findByUserId(Long.parseLong(userId));
+		Optional<Issue> issue = issuesRepository.findById(id);
+		if (issues.contains(issue.get())) {
+			issuesRepository.deleteById(id);
+		}
+		return new ResponseEntity<Issue>(HttpStatus.OK);
+	}
+
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	@GetMapping(path = "/getAllIssuesFor/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<Issue>> getUserIssues(@PathVariable(value = "userId") long userId) {
 		List<Issue> issues = issuesRepository.findByUserId(userId);
 		return new ResponseEntity<List<Issue>>(issues, HttpStatus.OK);
